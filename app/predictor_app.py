e3d23f873ed8c4fff409415dd01c094c9f66f821
Marie-Christine Meyer <75020248+macrst@users.noreply.github.com>
2023-01-26 08:22:44 +0100

    Update predictor_app.py

--- app/predictor_app.py
+++ app/predictor_app.py
@@ -41,7 +41,8 @@ st.write(
 ''')
 
 
-data = pd.read_pickle("../solar_clean.pickle")
+data = pickle.load(open('../solar_clean.pickle', 'rb'))
+#data = pd.read_pickle("../solar_clean.pickle")
 data = data.rename(columns={'brand_bucketed': 'brand',
                             'panel_type_bucketed': 'panel_type',
                             'region_bucketed': 'region',
